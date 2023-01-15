ca6f233a3d8b392b986096da1b7dba61ad105afd
Amon3m <ahmed.50011.am@gmail.com>
2023-01-15 20:37:48 +0200

    initial project

---
+++ app/src/main/java/com/m3/newsapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.m3.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
