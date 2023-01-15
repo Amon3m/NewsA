bcc601cb439fb543c2d77920ca15642795f63944
Amon3m <ahmed.50011.am@gmail.com>
2023-01-15 21:13:55 +0200

    create splash

--- app/src/main/java/com/m3/newsapp/HomeActivity.kt
+++ app/src/main/java/com/m3/newsapp/HomeActivity.kt
@@ -3,16 +3,9 @@ package com.m3.newsapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
-
-
-
-
+        setContentView(R.layout.activity_home)
     }
 }
