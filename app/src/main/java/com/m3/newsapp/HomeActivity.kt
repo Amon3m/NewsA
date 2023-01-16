a778d40399f4987d2af0159bf0f86080476d9346
Amon3m <ahmed.50011.am@gmail.com>
2023-01-16 16:07:35 +0200

    config news api get news from source

--- app/src/main/java/com/m3/newsapp/HomeActivity.kt
+++ app/src/main/java/com/m3/newsapp/HomeActivity.kt
@@ -4,9 +4,12 @@ import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.android.material.tabs.TabLayout
 import com.m3.islami2.base.BaseActivity
 import com.m3.newsapp.api.ApiManger
 import com.m3.newsapp.api.Constance
+import com.m3.newsapp.api.model.ArticlesItem
+import com.m3.newsapp.api.model.NewsResponse
 import com.m3.newsapp.api.model.SourceResponse
 import com.m3.newsapp.api.model.SourcesItem
 import com.m3.newsapp.databinding.ActivityHomeBinding
@@ -14,7 +17,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class HomeActivity : BaseActivity() {
+class HomeActivity : BaseActivity() ,TabLayout.OnTabSelectedListener{
     lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +36,7 @@ class HomeActivity : BaseActivity() {
                 binding.progressBar.visibility= View.GONE
 
                 if(response.isSuccessful){
-                    showsourceintablayout(response.body()?.sources)
+                    showSourceIntablayout(response.body()?.sources)
 
                 }
                 else{
@@ -59,12 +62,67 @@ class HomeActivity : BaseActivity() {
         })
     }
 
-    private fun showsourceintablayout(sources: List<SourcesItem?>?) {
+    private fun showSourceIntablayout(sources: List<SourcesItem?>?) {
 
         sources?.forEach{item->
+
             val tab=binding.tablayot.newTab()
+            tab.tag=item
             tab.text = item?.name
             binding.tablayot.addTab(tab)
         }
+        binding.tablayot.addOnTabSelectedListener(this)
     }
+
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        val item=tab?.tag as SourcesItem
+        getNews(item.id)
+    }
+
+    private fun getNews(sourceID: String?) {
+
+        ApiManger.getApis().getNews(Constance.apiKey,"en","",sourceID?:"")
+            .enqueue(object :Callback<NewsResponse>{
+                override fun onResponse(
+                    call: Call<NewsResponse>,
+                    response: Response<NewsResponse>
+                ) {
+                    if(response.isSuccessful){
+                        ShowNewsInRecyclerView(response.body()?.articles)
+                    }
+                    else{
+                        showDialoge(message = response.body()?.message?:"",
+                            posActionName = getString(R.string.ok)
+                            , posAction = DialogInterface.OnClickListener { dialog, which ->
+                                //  call.enqueue(this)
+                                call.clone().enqueue(this)
+                                dialog.dismiss()
+                            })}
+                }
+
+                override fun onFailure(call: Call<NewsResponse>, t: Throwable) {
+                    showDialoge(message = t.localizedMessage as String,
+                        posActionName = getString(R.string.retry)
+                        , posAction = DialogInterface.OnClickListener { dialog, which ->
+                            call.clone().enqueue(this)
+                            dialog.dismiss()
+                        })
+                }
+            })
+    }
+
+
+    //,,,,,,,,,,,,,,
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+    }
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
+    }
+
+
+}
+
+class ShowNewsInRecyclerView(articles: List<ArticlesItem?>?) {
+
 }
